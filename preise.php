97ecce6bee223a96c94033dcdd1a3da80c4601f2
Arso-Reinigung <158174949+Arso-Reinigung@users.noreply.github.com>
2024-01-30 18:22:29 +0100

    Delete preise.php

--- preise.php
+++
@@ -1,7 +0,0 @@
-﻿<div class="contentBlock">
-	<div class="daten">
-		<table width="400" style="table-layout: fixed;">
-			<tr><td nowrap >Unsere Preise</td><td ><a href="pdf/arso_preisliste.pdf?v=12345" target="_blank"><img src="img/pdf_icon.png" style="margin-left: 0px;"></img></a></td></tr>
-		</table>
-	</div>
-</div>
